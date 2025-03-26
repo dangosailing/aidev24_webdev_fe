a2a9a653a28d86b0481faa1f88774ff7608ddc5b
Cryxelia <luhuan.olsson@gmail.com>
2025-03-26 14:28:37 +0100

    changed in create-user-api

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -3,8 +3,8 @@ import axiosInstance from "./axiosInstance"
 
 export const createUser = async (userData) => {
     try{
-        const response = await axiosInstance.post("/users/create-user/", userData)
-        return response.message
+        const response = await axiosInstance.post("/users/create-user", userData)
+        return response.data
     }catch(error){
         console.error("Error creating user:", error)
         throw error
