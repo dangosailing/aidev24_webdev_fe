b4ebc4e9ed834196e89a06b59c45b2aab1cd0ce8
Daniel Salin <daniel.salin@gmail.com>
2025-03-27 17:41:03 +0100

    Add Account page

---
+++ src/pages/Account.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UpdateUser from "../components/UpdateUser";
+
+const Account = () => {
+  return (
+    <div>
+      <h1>Manage account</h1>
+      <UpdateUser />
+    </div>
+  );
+};
+
+export default Account;
