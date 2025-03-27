3e855a53de5fd3619a34d51547988561c01a2745
Daniel Salin <daniel.salin@gmail.com>
2025-03-27 17:46:52 +0100

    Add token validation to Account page, navigate to login if unauthorized

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import UpdateUser from "../components/UpdateUser";
 
 const Account = () => {
+  const [token, setToken] = useState("");
+  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const sessionToken = sessionStorage.getItem("token");
+    if (!sessionToken) {
+      navigate("/login");
+    } else {
+      setToken(sessionToken);
+    }
+  }, [navigate]);
+
   return (
     <div>
       <h1>Manage account</h1>
