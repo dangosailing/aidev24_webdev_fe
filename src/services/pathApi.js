99c6a3992374b2a96441ce6f45b3b23ed3c0f16e
Daniel Salin <daniel.salin@gmail.com>
2025-03-28 18:44:37 +0100

    Add savePath to pathApi, format file

--- src/services/pathApi.js
+++ src/services/pathApi.js
@@ -1,13 +1,21 @@
-import axiosInstance from "./axiosInstance"
+import axiosInstance from "./axiosInstance";
 
 export const newPath = async (pathData) => {
-    try {
-        const response = await axiosInstance.post("/paths/get-path", pathData)
-        console.log(response)
-        return response.data
-    } catch (error) {
-        console.error("Error geting new path", error)
-        throw error
-    }
+  try {
+    const response = await axiosInstance.post("/paths/get-path", pathData);
+    return response.data;
+  } catch (error) {
+    console.error("Error geting new path", error);
+    throw error;
+  }
+};
 
-}
+export const savePath = async (pathData) => {
+  try {
+    const response = await axiosInstance.post("/paths/save-path", pathData);
+    return response.data;
+  } catch (error) {
+    console.error("Error saving path", error);
+    throw error;
+  }
+};
