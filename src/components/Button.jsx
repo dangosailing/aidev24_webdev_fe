83dbbbaa7d150a496c8444b0b49c09872ffd7bbf
Daniel Salin <daniel.salin@gmail.com>
2025-03-28 19:23:54 +0100

    Add type prop to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({text, onClick}) => {
+const Button = ({text, type = "button", onClick}) => {
   return (
-    <button onClick={onClick}>
+    <button type={type} onClick={onClick}>
       {text}
     </button>
   )
