619e333bf835d906a5c7f837b7c4ef9f6715247b
Daniel Salin <daniel.salin@gmail.com>
2025-03-30 16:28:40 +0200

    hotfix: remove blank space in useState for servermessage " " -> ""

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,7 +4,7 @@ import UserContext from './UserContextBase';
     const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [serverMessage, setServerMessage] = useState(" ");
+    const [serverMessage, setServerMessage] = useState("");
 
     return (
         <UserContext.Provider value={{ user, setUser, isLoggedIn,setIsLoggedIn, serverMessage, setServerMessage }}>
