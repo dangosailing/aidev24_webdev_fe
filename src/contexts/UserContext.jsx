c7e2a44c0ee3634a79cb2a6574563be7c76e4d57
Nippe <niklaspettersson92@hotmail.com>
2025-03-30 14:05:53 +0200

    add setServerMessage in context

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,9 +4,10 @@ import UserContext from './UserContextBase';
     const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [serverMessage, setServerMessage] = useState(" ");
 
     return (
-        <UserContext.Provider value={{ user, setUser, isLoggedIn,setIsLoggedIn }}>
+        <UserContext.Provider value={{ user, setUser, isLoggedIn,setIsLoggedIn, serverMessage, setServerMessage }}>
             {children}
         </UserContext.Provider>
     );
