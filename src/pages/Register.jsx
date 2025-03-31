a44d78a3b706c83ec6379b13b1d9241966211d0f
Daniel Salin <daniel.salin@gmail.com>
2025-03-31 16:36:06 +0200

    Update Register page to work with form component and navigation. try/catch is now in form component, hence no try catch in onSubmit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
-import Form from '../components/Form'
-import { createUser } from '../services/userApi'
-
+import React from "react";
+import Form from "../components/Form";
+import { createUser } from "../services/userApi";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+      const response = await createUser(data);
+      navigate("/login");
+      return response;
+  };
+
   const fields = [
-    { name: "username", label: "Username", validation: { required: "Username is required" } },
-    { name: "password", label: "Password", type: "password", validation: { 
-        required: "Password is required", 
-        minLength: { value: 6, message: "Password must be at least 6 characters" } 
-      } 
-    }
-  ]
+    {
+      name: "username",
+      label: "Username",
+      validation: { required: "Username is required" },
+    },
+    {
+      name: "password",
+      label: "Password",
+      type: "password",
+      validation: {
+        required: "Password is required",
+        minLength: {
+          value: 6,
+          message: "Password must be at least 6 characters",
+        },
+      },
+    },
+  ];
 
   return (
     <div className="form-container">
       <h2>Create new user</h2>
-      <Form fields={fields} onSubmit={createUser} />
+      <Form fields={fields} onSubmit={onSubmit} />
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
