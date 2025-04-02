58db9a4bee663df3bc06bb1e9c5cd6e615557672
Daniel Salin <daniel.salin@gmail.com>
2025-04-02 20:12:56 +0200

    Add new tile layer options to PathMaker

--- src/pages/PathMaker.jsx
+++ src/pages/PathMaker.jsx
@@ -14,12 +14,14 @@ import Timer from "../components/Timer";
 import MapUpdater from "../components/UpdateMapPosition";
 import { savePath } from "../services/pathApi";
 import Form from "../components/Form";
-import Loading from "../components/Loading"
+import Loading from "../components/Loading";
+import Button from "../components/Button";
 import "../styles/PathMaker.css";
 
 const PathMaker = () => {
   const [token, setToken] = useState("");
   const [loading, setLoading] = useState(false);
+  const [mapStyle, setMapStyle] = useState("detailed");
 
   const {
     setTitle,
@@ -61,11 +63,25 @@ const PathMaker = () => {
     const response = await savePath(pathData);
     setTitle(data.title);
     setLoading(false);
-    return response
+    return response;
   };
 
   return (
     <div className="container">
+      <div className="container-map-style">
+        <Button
+          text={"Detailed"}
+          onClick={() => setMapStyle("detailed")}
+        ></Button>
+        <Button
+          text={"Simplified"}
+          onClick={() => setMapStyle("simple")}
+        ></Button>
+        <Button
+          text={"Dark Mode"}
+          onClick={() => setMapStyle("darkmode")}
+        ></Button>
+      </div>
       <div className="pathmaker-grid">
         <div className="map">
           <MapContainer
@@ -74,10 +90,24 @@ const PathMaker = () => {
             scrollWheelZoom={true}
             style={{ height: "500px", width: "100%", borderRadius: "8px" }}
           >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+            {mapStyle === "detailed" && (
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            )}
+            {mapStyle === "simple" && (
+              <TileLayer
+                attribution='&copy; <a href="https://carto.com/">CARTO</a> contributors'
+                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+              />
+            )}
+            {mapStyle === "darkmode" && (
+              <TileLayer
+                attribution='&copy; <a href="https://carto.com/">CARTO</a> contributors'
+                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              />
+            )}
             <MapUpdater route={route} />
 
             {route.length > 0 && (
@@ -89,20 +119,22 @@ const PathMaker = () => {
             {route.length > 0 && <Polyline positions={route} color="blue" />}
           </MapContainer>
         </div>
-        { !loading && (
-        <div className="container-position-save">
-          <GetUserPosition
-            onPositionUpdate={setPosition}
-            onRouteUpdate={setRoute}
-          />
-          {route && route.length > 0 && (
-              <Form fields={fields} onSubmit={onSubmit} buttonText="Save path"/>
-            
-          )}
-        </div>)
-        }
-        {loading &&
-        <Loading />        }
+        {!loading && (
+          <div className="container-position-save">
+            <GetUserPosition
+              onPositionUpdate={setPosition}
+              onRouteUpdate={setRoute}
+            />
+            {route && route.length > 0 && (
+              <Form
+                fields={fields}
+                onSubmit={onSubmit}
+                buttonText="Save path"
+              />
+            )}
+          </div>
+        )}
+        {loading && <Loading />}
 
         <Timer />
       </div>
