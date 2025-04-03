81fe4adbbac4004222530b329d97a107910bb8d2
daniel-salin <daniel.salin@gmail.com>
2025-04-03 16:23:41 +0200

    hotfix:working test for login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,22 +1,23 @@
 describe('Login Test', () => {
-    it('should login successfully and set the token cookie', () => {
+    it('should login successfully and ', () => {
         cy.visit('http://localhost:5173/register')
 
-        cy.get("input[name='username']").type('hejhå')
+        cy.get("input[name='username']").type('testUser')
         cy.get("input[name='password']").type('123456')
 
         cy.get("button[type='submit']").click()
 
-        cy.wait(500)
+        cy.wait(1000)
 
         cy.visit('http://localhost:5173/login')
 
-        cy.get("input[name='username']").type('1234')
+        cy.get("input[name='username']").type('testUser')
         cy.get("input[name='password']").type('123456')
 
         cy.get("button[type='submit']").click()
 
-        cy.wait(500)
+        cy.wait(1000)
         cy.visit('http://localhost:5173/profile')
+        cy.get('#test-header').should('contain', 'Welcome, testUser!')
     })
 })
