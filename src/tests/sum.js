d0477f18120d6c9408b51108d3a75baf436c9117
Daniel Salin <daniel.salin@gmail.com>
2025-04-04 17:34:21 +0200

    hotfix: remove jest since it´s unused

--- src/tests/sum.js
+++
@@ -1,6 +0,0 @@
-// Made this to demonstrate a simple test function from the jest docs. This is not intended to be implemented in the dev branch!
-
-function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
